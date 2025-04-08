76091416900e1eb80dc7ea57236fb823c54570c1
Max Heinze <maxheinze@googlemail.com>
2025-04-08 13:06:51 +0200

    [+] WIP Cpp EnemySpawner

---
+++ Source/ProphecyOfAsh/EnemySpawner.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EnemySpawner.generated.h"
+
+UCLASS()
+class PROPHECYOFASH_API AEnemySpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEnemySpawner();
+
+	UFUNCTION(BlueprintCallable, Category = "Enemy Spawner")
+	void EnemyKilled(AActor* DeadEnemy);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	bool SpawnEnemy();
+	bool ReSpawnEnemy(AActor* Enemy);
+
+	bool SpawnEnemy(AActor* Enemy);
+
+	void RespawnEnemy();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Spawner")
+	TSubclassOf<AActor> EnemyClass;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Spawner")
+	float RespawnTimer = 5.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Spawner")
+	int NumberToSpawn = 2;
+
+	TArray<AActor*> AliveEnemies;
+	TArray<AActor*> AwaitingRespawn;
+	TArray<FTimerHandle> RespawnTimers;
+};
