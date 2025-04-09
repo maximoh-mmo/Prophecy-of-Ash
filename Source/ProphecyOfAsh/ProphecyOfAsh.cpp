3a2d9b241ad3bcdcc53f2b1f84a0a0aaa5d654a0
Zarbony <jenskaerger95@gmail.com>
2025-04-09 15:53:56 +0200

    [+] Add StartMenu Map [+] Add Pause Menu [+] Add Main Menu [~] Edit HUD

---
+++ Source/ProphecyOfAsh/ProphecyOfAsh.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProphecyOfAsh.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProphecyOfAsh, "ProphecyOfAsh" );
