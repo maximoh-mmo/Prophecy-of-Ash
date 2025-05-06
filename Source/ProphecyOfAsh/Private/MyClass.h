e4a7ce989880dd53e36229be3a6bfd50d67f729e
Zarbony <jenskaerger95@gmail.com>
2025-05-06 15:26:00 +0200

    [~] Repair Pause Menu Function

--- Source/ProphecyOfAsh/Private/MyClass.h
+++ Source/ProphecyOfAsh/Private/MyClass.h
@@ -5,7 +5,11 @@
 #include "CoreMinimal.h"
 
 /**
- * 
+ * @class MyClass
+ * @brief Represents a simple, default constructible and destructible class.
+ *
+ * Provides a basic structure for creating and destroying instances.
+ * Serves as a foundational example or base for further extension.
  */
 class MyClass
 {
