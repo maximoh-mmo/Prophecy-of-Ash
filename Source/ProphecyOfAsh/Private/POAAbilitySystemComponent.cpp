ef1f54e017ef137b8589d4df4bcde2ef103c5047
Max Heinze <maxheinze@googlemail.com>
2025-05-07 11:28:36 +0200

    [+] Start on Ability System

---
+++ Source/ProphecyOfAsh/Private/POAAbilitySystemComponent.cpp
@@ -0,0 +1,12 @@
+//
+// Copyright 2025 Max Heinze. All Rights Reserved.
+//
+// This file is part of the Prophecy of Ash project.
+
+#include "POAAbilitySystemComponent.h"
+
+
+// Sets default values for this component's properties
+UPOAAbilitySystemComponent::UPOAAbilitySystemComponent()
+{
+}
