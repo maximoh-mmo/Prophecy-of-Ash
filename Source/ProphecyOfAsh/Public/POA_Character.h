62be975ba12fa213d0910cc098d33f58283d39d5
Max Heinze <maxheinze@googlemail.com>
2025-05-16 09:49:52 +0200

    [*] Work on character base class

--- Source/ProphecyOfAsh/Public/POA_Character.h
+++ Source/ProphecyOfAsh/Public/POA_Character.h
@@ -7,8 +7,6 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "Logging/LogMacros.h"
-#include "EnhancedInputSubsystems.h"
-#include "EnhancedInputComponent.h"
 #include "POA_Character.generated.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(LogTemplateCharacter, Log, All);
@@ -81,7 +79,13 @@ protected:
 
 private:
 	UPROPERTY()
-	float MaxHealth = 100.f;
+	float MaxHealth;
 	UPROPERTY()
-	float CurrentHealth = 100.f;
+	float CurrentHealth;
+	UPROPERTY()
+	int Vigor;
+	UPROPERTY()
+	int Power;
+	UPROPERTY()
+	int Control;
 };
