70545b25f773bfc91a7d9acaec041e28a85ca8bc
Giuseppe <napoligiuseppe@web.de>
2025-08-06 00:33:33 +0200

    Character ready Combat Feedback Implementation

--- Source/ProphecyOfAsh/Private/POA_CombatFeedbackSubsystem.cpp
+++ Source/ProphecyOfAsh/Private/POA_CombatFeedbackSubsystem.cpp
@@ -3,12 +3,22 @@
 
 #include "POA_CombatFeedbackSubsystem.h"
 
+#include "CombatFeedbackComponent.h"
+
 void UPOA_CombatFeedbackSubsystem::TriggerFeedback(const FGameplayTag FeedbackTag, AActor* InstigatorActor, AActor* TargetActor)
 {
 	UE_LOG(LogTemp, Log, TEXT("TriggerFeedback: %s | Instigator: %s | Target: %s"),
 		*FeedbackTag.ToString(),
 		*GetNameSafe(InstigatorActor),
 		*GetNameSafe(TargetActor));
+
+	if (IsValid(TargetActor))
+	{
+		if (UCombatFeedbackComponent* FeedbackComp = TargetActor->FindComponentByClass<UCombatFeedbackComponent>())
+		{
+			FeedbackComp->HandleFeedback_BP(FeedbackTag, InstigatorActor, TargetActor);
+		}
+	}
 	HandleFeedback_BP(FeedbackTag, InstigatorActor, TargetActor);
 }
 
@@ -22,3 +32,5 @@ void UPOA_CombatFeedbackSubsystem::TriggerFeedbackMultiple(const FGameplayTag Fe
 		}
 	}
 }
+
+
